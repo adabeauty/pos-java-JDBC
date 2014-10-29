06861eb28ce76fe131998e146f870df62aa58b1d
adabeauty <654296217@qq.com>
2014-10-29 15:53:32 +0800

    implement getCategoryById function

--- src/main/java/com/thoughtworks/iamcoach/pos/Dao/CategoryImple.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Dao/CategoryImple.java
@@ -1,7 +1,45 @@
 package com.thoughtworks.iamcoach.pos.Dao;
 
-/**
- * Created by wanghuan on 14-10-29.
- */
-public class CategoryImple {
+import com.thoughtworks.iamcoach.pos.Ulti.Ulti;
+import com.thoughtworks.iamcoach.pos.module.Category;
+import com.thoughtworks.iamcoach.pos.module.Item;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CategoryImple implements CategoryDao{
+    private Ulti ulti= new Ulti();
+    
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    
+    @Override
+    public ArrayList<Category> getCategories() {
+        return null;
+    }
+
+    @Override
+    public Category getCategoryById(int id) {
+        String sql = "SELECT * FROM items WHERE id = ?";
+        Category category = null;
+        Connection conn = ulti.getConnection();
+        try{
+            preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery(sql);
+            resultSet.next();
+            category = new Category(resultSet.getString("name"));
+
+            ulti.closeConnection();
+            preparedStatement.close();
+            resultSet.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return category;
+    }
 }
