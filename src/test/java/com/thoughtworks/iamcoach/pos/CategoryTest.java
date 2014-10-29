68b5ac72154d08f96c414b9d2f457ef28ddb8728
adabeauty <654296217@qq.com>
2014-10-29 12:40:03 +0800

    finish can_get_category_paraments test

--- src/test/java/com/thoughtworks/iamcoach/pos/CategoryTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/CategoryTest.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.iamcoach.pos;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
  * Created by wanghuan on 14-10-29.
  */
 public class CategoryTest {
+    @Test
+    public void can_get_category_paraments(){
+        String name = "drink";
+        Category category = new Category(name);
+
+        assertThat(category.getId()).isEqualTo(0);
+        assertThat(category.getName()).isEqualTo(name);
+    }
 }
