79e0951c8944573f65c31509d4adabf6916d7f5d
adabeauty <654296217@qq.com>
2014-10-29 23:16:09 +0800

    mock ItemImple class

--- src/test/java/com/thoughtworks/iamcoach/pos/service/ItemServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/ItemServiceTest.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.Dao.ItemImple;
 import com.thoughtworks.iamcoach.pos.module.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ItemServiceTest {
     @Test
@@ -35,4 +40,25 @@ public class ItemServiceTest {
         Category category = new Category("fruit");
         assertThat(itemService.getCategory(id).toString()).isEqualTo(category.toString());
     }
+
+    private void mock_ItemImple(){
+        ArrayList<Item> items = new ArrayList<Item>();
+        Item item = new Item(3,"TF1001", "apple", "kg", 8.0);
+        items.add(item);
+
+        ArrayList<Promotion> promotions = new ArrayList<Promotion>();
+        Promotion promotion = new Promotion(1, 1, "buy_two_one_free");
+        promotions.add(promotion);
+
+        String barcode = null;
+        int id = 1;
+        Category category = new Category("fruit");
+        
+        ItemImple itemImple = mock(ItemImple.class);
+        when(itemImple.getItems()).thenReturn(items);
+        when(itemImple.getItemByBarcode(barcode)).thenReturn(item);
+        when(itemImple.getPromotions(id)).thenReturn(promotions);
+        when(itemImple.getCategory(id)).thenReturn(category);
+    }
+
 }
