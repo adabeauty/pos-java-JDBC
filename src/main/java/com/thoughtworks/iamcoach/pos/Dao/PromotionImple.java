5e8e21a350146241964b92cadf9e1431867fad87
adabeauty <654296217@qq.com>
2014-10-29 14:46:35 +0800

    implement PromotionDao interface

--- src/main/java/com/thoughtworks/iamcoach/pos/Dao/PromotionImple.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Dao/PromotionImple.java
@@ -1,7 +1,20 @@
 package com.thoughtworks.iamcoach.pos.Dao;
 
+import com.thoughtworks.iamcoach.pos.module.Promotion;
+
+import java.util.ArrayList;
+
 /**
  * Created by wanghuan on 14-10-29.
  */
-public class PromotionImple {
+public class PromotionImple implements PromotionDao {
+    @Override
+    public Promotion getPromotionByType(int type) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Promotion> getPromotions() {
+        return null;
+    }
 }
