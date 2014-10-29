1e58a5945d36af3b0db1a032c8f253ccede6f1af
adabeauty <654296217@qq.com>
2014-10-29 16:09:13 +0800

    rename Ulti to ConnectionUlti

--- src/main/java/com/thoughtworks/iamcoach/pos/Ulti/ConnctionUlti.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Ulti/ConnctionUlti.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Ulti {
+public class ConnctionUlti {
 
     private String driverName = "com.mysql.jdbc.Driver";
     private String url = "jdbc:mysql://localhost:3306/pos_java?useUnicode=true&characterEncoding=utf-8";   //pos是database名字
