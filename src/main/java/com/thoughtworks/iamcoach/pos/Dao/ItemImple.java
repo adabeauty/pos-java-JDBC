d2aca4c0a29305693f1cb3e97e62b7a12f467727
adabeauty <654296217@qq.com>
2014-10-29 14:33:32 +0800

    achieve getItemByBarcode

--- src/main/java/com/thoughtworks/iamcoach/pos/Dao/ItemImple.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Dao/ItemImple.java
@@ -1,16 +1,38 @@
 package com.thoughtworks.iamcoach.pos.Dao;
 
 import com.thoughtworks.iamcoach.pos.Category;
+import com.thoughtworks.iamcoach.pos.Ulti.Ulti;
 import com.thoughtworks.iamcoach.pos.module.Item;
 import com.thoughtworks.iamcoach.pos.module.Promotion;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ItemImple implements ItemDao {
+    private PreparedStatement pstmt = null;
+    private ResultSet rs = null;
+    private Ulti ulti= new Ulti();
 
     @Override
     public Item getItemByBarcode(String barcode) {
-        return null;
+        String sql = "SELECT * FROM items WHERE barcode = ?";
+        Item item = null;
+        Connection conn = ulti.getConnection();
+        try{
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, barcode);
+            rs = pstmt.executeQuery(sql);
+            rs.next();
+            item = new Item(rs.getInt("categoryId"), rs.getString("barcode"), rs.getString("name"), rs.getString("unit"), rs.getDouble("price"));
+
+            ulti.closeConnection();
+            pstmt.close();
+            rs.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return item;
     }
 
     @Override
