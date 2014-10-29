07a7a0b03773d892edabe206e9f1b8da5d8174ec
adabeauty <654296217@qq.com>
2014-10-29 16:15:18 +0800

    add readFile readFileUlti class

--- src/main/java/com/thoughtworks/iamcoach/pos/Ulti/ReadFileUlti.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Ulti/ReadFileUlti.java
@@ -1,7 +1,25 @@
 package com.thoughtworks.iamcoach.pos.Ulti;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 /**
  * Created by wanghuan on 14-10-29.
  */
 public class ReadFileUlti {
+    public ArrayList<String> readFile(String fileName){
+        ArrayList<String> text = new ArrayList<String>();
+
+        try {
+            Path path = FileSystems.getDefault().getPath("src/main/resources/", fileName);
+            text = (ArrayList<String>) Files.readAllLines(path);
+        } catch (IOException ex) {
+            System.out.println("fail read file!");
+        }
+
+        return text;
+    }
 }
