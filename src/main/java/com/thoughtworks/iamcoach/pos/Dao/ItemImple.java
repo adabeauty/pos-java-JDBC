31f88d56c6df86991961c9d396e2d2dee7d4a2e7
adabeauty <654296217@qq.com>
2014-10-29 14:31:30 +0800

    implement ItemDao interface

--- src/main/java/com/thoughtworks/iamcoach/pos/Dao/ItemImple.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Dao/ItemImple.java
@@ -1,7 +1,30 @@
 package com.thoughtworks.iamcoach.pos.Dao;
 
-/**
- * Created by wanghuan on 14-10-29.
- */
-public class ItemImple {
+import com.thoughtworks.iamcoach.pos.Category;
+import com.thoughtworks.iamcoach.pos.module.Item;
+import com.thoughtworks.iamcoach.pos.module.Promotion;
+
+import java.util.ArrayList;
+
+public class ItemImple implements ItemDao {
+
+    @Override
+    public Item getItemByBarcode(String barcode) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<ItemDao> getItems() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Promotion> getPromotionOfItem(int id) {
+        return null;
+    }
+
+    @Override
+    public Category getCategory(int id) {
+        return null;
+    }
 }
