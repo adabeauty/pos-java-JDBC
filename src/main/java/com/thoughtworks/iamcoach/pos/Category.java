bb306346c78f6146fc93596643d729226609ae5a
adabeauty <654296217@qq.com>
2014-10-29 12:40:33 +0800

    create getter and constructor

--- src/main/java/com/thoughtworks/iamcoach/pos/Category.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Category.java
@@ -4,4 +4,18 @@ package com.thoughtworks.iamcoach.pos;
  * Created by wanghuan on 14-10-29.
  */
 public class Category {
+    private int id;
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
