dd85251cb704d7c45ce771d3fe6e0de284817069
adabeauty <654296217@qq.com>
2014-10-29 17:27:33 +0800

    finish test and function of getItems

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -1,7 +1,16 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.Dao.ItemImple;
+import com.thoughtworks.iamcoach.pos.module.Item;
+
+import java.util.ArrayList;
+
 /**
  * Created by wanghuan on 14-10-29.
  */
 public class ItemService {
+    ItemImple itemImple = new ItemImple();
+    public ArrayList<Item> getItems() {
+        return itemImple.getItems();
+    }
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/service/ItemServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/ItemServiceTest.java
@@ -1,7 +1,14 @@
 package com.thoughtworks.iamcoach.pos.service;
 
-/**
- * Created by wanghuan on 14-10-29.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class ItemServiceTest {
+    @Test
+    public void can_get_all_items(){
+        ItemService itemService = new ItemService();
+        assertThat(itemService.getItems().size()).isEqualTo(5);
+    }
+
 }
