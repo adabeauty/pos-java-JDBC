aa52a837a4952123456dac3b18d896bdc39675e4
adabeauty <654296217@qq.com>
2014-10-29 14:12:15 +0800

    add toString function

--- src/main/java/com/thoughtworks/iamcoach/pos/Category.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Category.java
@@ -18,4 +18,12 @@ public class Category {
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
