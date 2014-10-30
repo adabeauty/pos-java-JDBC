3a8bd75ebd207b9d0299eeeeca28e081ab7b0142
adabeauty <654296217@qq.com>
2014-10-30 16:44:27 +0800

    finish getPromotionByType function

--- src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
@@ -1,7 +1,16 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.Dao.PromotionDao;
+import com.thoughtworks.iamcoach.pos.Dao.PromotionImple;
+import com.thoughtworks.iamcoach.pos.module.Promotion;
+
 /**
  * Created by wanghuan on 14-10-30.
  */
 public class PromotionService {
+    private PromotionDao promotionImple = new PromotionImple();
+
+    public Promotion getPromotionByType(int type) {
+        return promotionImple.getPromotionByType(type);
+    }
 }
