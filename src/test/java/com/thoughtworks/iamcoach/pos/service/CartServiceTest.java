47e7f1d9dd71fe0d7a02dd80e890dcc00b427be3
adabeauty <654296217@qq.com>
2014-10-30 19:34:36 +0800

    add boundary test to can_get_all_cartItems

--- src/test/java/com/thoughtworks/iamcoach/pos/service/CartServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/CartServiceTest.java
@@ -10,5 +10,7 @@ public class CartServiceTest {
     public void can_get_all_cartItems(){
         CartService cartService = new CartService();
         assertThat(cartService.getCartInfo().size()).isEqualTo(3);
+        assertThat(cartService.getCartInfo().get(0).getNum()).isEqualTo(5);
+        assertThat(cartService.getCartInfo().get(1).getNum()).isEqualTo(2.0);
     }
 }
