bd774a274adbdf6128e3df8f1105b38f94dfebda
adabeauty <654296217@qq.com>
2014-10-30 20:39:50 +0800

    modify categoryId parament

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -9,7 +9,7 @@ public class PrinterTest {
     @Test
     public void can_divide_cartItems(){
         Printer printer = new Printer();
-        int categoryId = 1;
+        int categoryId = 3;
         assertThat(printer.divideCartItems(categoryId).size()).isEqualTo(2);
     }
 }
