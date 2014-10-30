8737e4ed3dd8bb1e0ba29752d9972918de7aeb6e
adabeauty <654296217@qq.com>
2014-10-30 19:16:52 +0800

    finish can_get_all_cartItems

--- src/test/java/com/thoughtworks/iamcoach/pos/service/CartServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/CartServiceTest.java
@@ -1,7 +1,22 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
  * Created by wanghuan on 14-10-30.
  */
 public class CartServiceTest {
+
+    @Test
+    public void can_get_all_cartItems(){
+        ArrayList<String> inputs = null;
+        ArrayList<String> items = null;
+        CartService cartService = new CartService();
+
+        assertThat(cartService.getCartInfo().size()).isEqualTo(3);
+    }
 }
