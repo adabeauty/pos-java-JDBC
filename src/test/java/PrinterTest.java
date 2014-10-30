43cac51eb6fadbb10d2790ee4f06c2aab67a8770
adabeauty <654296217@qq.com>
2014-10-30 20:40:39 +0800

    delete useless codes

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -2,9 +2,6 @@ import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
-/**
- * Created by wanghuan on 14-10-30.
- */
 public class PrinterTest {
     @Test
     public void can_divide_cartItems(){
