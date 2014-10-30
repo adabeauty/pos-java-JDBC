02f6684d874ec68f497c9841233d5057e85e3143
adabeauty <654296217@qq.com>
2014-10-30 20:40:11 +0800

    finish divideCartITtems function

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,3 +1,20 @@
+import com.thoughtworks.iamcoach.pos.module.CartItem;
+import com.thoughtworks.iamcoach.pos.service.CartService;
+
+import java.util.ArrayList;
 
 public class Printer {
+    public ArrayList<CartItem> divideCartItems(int categoryId) {
+        ArrayList<CartItem> dividedCartItems = new ArrayList<CartItem>();
+
+        CartService cartService = new CartService();
+        ArrayList<CartItem> cartItems = cartService.getCartInfo();
+
+        for(int i=0; i<cartItems.size(); i++){
+            if(cartItems.get(i).getCategoryId() == categoryId){
+                dividedCartItems.add(cartItems.get(i));
+            }
+        }
+        return dividedCartItems;
+    }
 }
