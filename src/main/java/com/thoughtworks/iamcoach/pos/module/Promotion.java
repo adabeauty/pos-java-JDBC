6ef3d027d8b0a6abd102aa0310859d9b3465f113
adabeauty <654296217@qq.com>
2014-10-30 11:12:03 +0800

    add discount to Promotion

--- src/main/java/com/thoughtworks/iamcoach/pos/module/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/module/Promotion.java
@@ -5,14 +5,16 @@ public abstract class Promotion {
     private int id;
     private int type;
     private String description;
+    private double discount;
 
     protected Promotion() {
     }
 
-    public Promotion(int id, int type, String description) {
+    public Promotion(int id, int type, String description, double discount) {
         this.id = id;
         this.type = type;
         this.description = description;
+        this.discount = discount;
     }
 
     public int getId() {
@@ -38,6 +40,16 @@ public abstract class Promotion {
     public void setDescription(String description) {
         this.description = description;
     }
+
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
     @Override
     public String toString() {
         return "Promotion{" +
