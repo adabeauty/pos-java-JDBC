c7a4c5e2fc3c56bb88c59a68cdbabea188442932
adabeauty <654296217@qq.com>
2014-10-30 17:30:43 +0800

    finish setter and getter of num

--- src/main/java/com/thoughtworks/iamcoach/pos/module/CartItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/module/CartItem.java
@@ -4,4 +4,13 @@ package com.thoughtworks.iamcoach.pos.module;
  * Created by wanghuan on 14-10-30.
  */
 public class CartItem {
+    private double num;
+
+    public double getNum() {
+        return this.num;
+    }
+
+    public void setNum(double num) {
+        this.num  = num;
+    }
 }
