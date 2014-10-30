edcfa763e80fc110b52e68ee9c7d390f3092e9d5
adabeauty <654296217@qq.com>
2014-10-30 17:31:11 +0800

    test setter and getter of num

--- src/test/java/com/thoughtworks/iamcoach/pos/module/CartItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/module/CartItemTest.java
@@ -2,10 +2,17 @@ package com.thoughtworks.iamcoach.pos.module;
 
 import org.junit.Test;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
  * Created by wanghuan on 14-10-30.
  */
 public class CartItemTest {
     @Test
-    public void
+    public void can_get_num(){
+        CartItem cartItem = new CartItem();
+        double num  = 9.0;
+        cartItem.setNum(num);
+        assertThat(cartItem.getNum()).isEqualTo(9.0);
+    }
 }
