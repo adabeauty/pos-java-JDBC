637f2084329e75ac7aec58caa990a84581b32bd7
adabeauty <654296217@qq.com>
2014-10-30 16:54:01 +0800

    add mock_PromotionImple private function

--- src/test/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceTest.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.iamcoach.pos.service;
 
-import com.thoughtworks.iamcoach.pos.module.BuyTwoOneFreePromotion;
+import com.thoughtworks.iamcoach.pos.Dao.PromotionDao;
+import com.thoughtworks.iamcoach.pos.Dao.PromotionImple;
+import com.thoughtworks.iamcoach.pos.module.Promotion;
+import com.thoughtworks.iamcoach.pos.module.SeconHalfPromotion;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by wanghuan on 14-10-30.
@@ -12,8 +17,17 @@ public class PromotionServiceTest {
     @Test
     public void can_get_promotion_by_type(){
         PromotionService promotionService = new PromotionService();
+        promotionService.setPromotionImple(mock_PromotionImple());
         int type = 1;
-        assertThat(promotionService.getPromotionByType(type) instanceof BuyTwoOneFreePromotion).isTrue();
+        assertThat(promotionService.getPromotionByType(type) instanceof SeconHalfPromotion).isTrue();
     }
 
+    private PromotionDao mock_PromotionImple(){
+        int type = 1;
+        Promotion promotion = new SeconHalfPromotion();
+
+        PromotionDao promotionImple = mock(PromotionImple.class);
+        when(promotionImple.getPromotionByType(type)).thenReturn(promotion);
+        return promotionImple;
+    }
 }
