1ab81ff0bc79fec589d344f2ba8af8b5a1a18b2a
adabeauty <654296217@qq.com>
2014-10-30 17:52:58 +0800

    add promotionList filed

--- src/main/java/com/thoughtworks/iamcoach/pos/module/CartItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/module/CartItem.java
@@ -1,7 +1,17 @@
 package com.thoughtworks.iamcoach.pos.module;
 
-public class CartItem {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CartItem extends Item{
     private double num;
+    private List<Promotion> promotionList = new ArrayList<Promotion>();
+
+    public CartItem(Item item, double num) {
+        super(item.getCategoryId(), item.getBarcode(), item.getName(), item.getUnit(), item.getPrice());
+        this.num = num;
+    }
 
     public double getNum() {
         return this.num;
@@ -9,5 +19,37 @@ public class CartItem {
 
     public void setNum(double num) {
         this.num  = num;
+    }
+
+    public List<Promotion> getPromotionList() {
+        return promotionList;
+    }
+
+    public void setPromotionList(List<Promotion> promotionList) {
+        this.promotionList = promotionList;
+    }
+    public double getSubtotal() {
+        ArrayList<Double> allSubTotals = new ArrayList<Double>();
+
+        Item item = new Item(this.getCategoryId(), this.getBarcode(), this.getName(), this.getUnit(), this.getPrice());
+        CartItem cartItem = new CartItem(item, this.num);
+
+        for(int i=0; i<getPromotion().size(); i++){
+            if(getPromotion().get(i).equals("buy_two_get_one_free_promotion")){
+                BuyTwoOneFreeCalculator buyTwoOneFreeCalculator = new BuyTwoOneFreeCalculator();
+                allSubTotals.add(buyTwoOneFreeCalculator.calculatePromotion(cartItem));
+            }
+            if(getPromotion().get(i).equals("second_half_price_promotion")){
+                SecondHalfCalculator secondHalfCalculator = new SecondHalfCalculator();
+                allSubTotals.add(secondHalfCalculator.calculatePromotion(cartItem));
+            }
+            if(getPromotion().get(i).equals("discount_promotion")){
+                DiscountCalculator discountCalculator = new DiscountCalculator();
+                allSubTotals.add(discountCalculator.calculatePromotion(cartItem));
+            }
+        }
+
+
+        return Collections.min(allSubTotals);
     }
 }
