64f7fcc2f8efef04311b3f29c505706ffb82190d
adabeauty <654296217@qq.com>
2014-10-30 16:36:18 +0800

    finish can_get_promotion_by_type test

--- src/test/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceTest.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.module.BuyTwoOneFreePromotion;
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
  * Created by wanghuan on 14-10-30.
  */
 public class PromotionServiceTest {
+    @Test
+    public void can_get_promotion_by_type(){
+        PromotionService promotionService = new PromotionService();
+        int type = 1;
+        assertThat(promotionService.getPromotionByType(type) instanceof BuyTwoOneFreePromotion).isTrue();
+    }
+
 }
