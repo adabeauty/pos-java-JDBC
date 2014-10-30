8862794f26aeee8e386b436f07d5942fc59b9596
adabeauty <654296217@qq.com>
2014-10-30 20:26:33 +0800

    finish can_divide_cartItems

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
  * Created by wanghuan on 14-10-30.
  */
 public class PrinterTest {
+    @Test
+    public void can_divide_cartItems(){
+        Printer printer = new Printer();
+        int categoryId = 1;
+        assertThat(printer.divideCartItems(categoryId).size()).isEqualTo(2);
+    }
 }
