53a108271c3dd23be06d75ce55685b08f097df06
adabeauty <654296217@qq.com>
2014-10-30 17:13:12 +0800

    finish ScannerTest

--- src/test/java/com/thoughtworks/iamcoach/pos/service/ScannerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/ScannerTest.java
@@ -1,7 +1,17 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+
 /**
  * Created by wanghuan on 14-10-30.
  */
 public class ScannerTest {
+    @Test
+    public void can_get_inputs(){
+        Scanner scanner = new Scanner();
+        assertThat(scanner.getInputs().size()).isEqualTo(9);
+    }
 }
