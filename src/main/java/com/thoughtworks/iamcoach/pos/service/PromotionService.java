4aa191322eb239de53623a1d9c640bf838c1e4ab
adabeauty <654296217@qq.com>
2014-10-30 16:46:20 +0800

    add setter of PromotionImple

--- src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
@@ -10,6 +10,10 @@ import com.thoughtworks.iamcoach.pos.module.Promotion;
 public class PromotionService {
     private PromotionDao promotionImple = new PromotionImple();
 
+    public void setPromotionImple(PromotionDao promotionImple) {
+        this.promotionImple = promotionImple;
+    }
+    
     public Promotion getPromotionByType(int type) {
         return promotionImple.getPromotionByType(type);
     }
