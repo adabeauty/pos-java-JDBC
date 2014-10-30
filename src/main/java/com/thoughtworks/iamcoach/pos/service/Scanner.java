bb172c5667c92e5c9646a30a1a33ee8785060eed
adabeauty <654296217@qq.com>
2014-10-30 17:12:15 +0800

    finish getInputs function

--- src/main/java/com/thoughtworks/iamcoach/pos/service/Scanner.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/Scanner.java
@@ -1,7 +1,15 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.Ulti.ReadFileUlti;
+
+import java.util.ArrayList;
+
 /**
  * Created by wanghuan on 14-10-30.
  */
 public class Scanner {
+    public ArrayList<String> getInputs() {
+        ReadFileUlti readFileUlti = new ReadFileUlti();
+        return readFileUlti.readFile("cart.txt");
+    }
 }
