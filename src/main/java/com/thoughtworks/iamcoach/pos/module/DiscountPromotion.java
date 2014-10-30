edc721993d263e76d8f0dbbf6d42bd34dd0b122f
adabeauty <654296217@qq.com>
2014-10-30 11:13:05 +0800

    get discount from father class

--- src/main/java/com/thoughtworks/iamcoach/pos/module/DiscountPromotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/module/DiscountPromotion.java
@@ -1,18 +1,9 @@
 package com.thoughtworks.iamcoach.pos.module;
 
 public class DiscountPromotion extends Promotion{
-    private double discount;
-
-    public DiscountPromotion() {
-    }
-
-    public DiscountPromotion(int id, int type, String description, double discount) {
-        super(id, type, description);
-        this.discount = discount;
-    }
 
     @Override
     public double calcultaPromotion(Item item, double num) {
-        return item.getPrice() * num * discount;
+        return item.getPrice() * num * getDiscount();
     }
 }
